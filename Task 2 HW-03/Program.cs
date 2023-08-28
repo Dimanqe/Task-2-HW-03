3486dc89bdedd217b31e45d17448f57422bd062c
Dimanqe <d.sankeev@gmail.com>
2023-08-29 00:33:05 +0300

    Добавьте файлы проекта.

---
+++ Task 2 HW-03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_HW_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string URL = @"C:\\Users\\dsank\\Desktop\\Test";
+
+            Console.WriteLine(GetDirectorySize(URL));
+
+        }
+
+        static long GetDirectorySize(string path)
+        {
+            long size = 0;
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                foreach (FileInfo file in directoryInfo.GetFiles())
+                {
+                    size += file.Length;
+                }
+                foreach (DirectoryInfo subDirectory in directoryInfo.GetDirectories())
+                {
+                    size += GetDirectorySize(subDirectory.FullName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при получении размера папки: {ex.Message}");
+            }
+            return size;
+        }
+    }
+}
